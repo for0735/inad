2ec7f2fb81577b849d63b72c9c2c887187aa7dcd
for0735 <wjd_wogns@naver.com>
2021-10-31 15:57:58 +0900

    주소 배열로 받아서 파싱작업

--- src/main/java/com/inad/mgr/controller/mapController.java
+++ src/main/java/com/inad/mgr/controller/mapController.java
@@ -33,6 +33,42 @@ public class mapController {
 		
 		int result = 0;
 		String addr = request.getParameter("addr");
+		String[] addrArr = addr.split(" ");
+		
+		//광역시로 변환
+		if(addrArr[0].equals("서울")) {
+			addrArr[0] = "서울특별시";
+		} else if(addrArr[0].equals("경기")) {
+			addrArr[0] = "경기도";
+		} else if(addrArr[0].equals("부산")) {
+			addrArr[0] = "부산광역시";
+		} else if(addrArr[0].equals("인천")) {
+			addrArr[0] = "인천광역시";
+		} else if(addrArr[0].equals("대구")) {
+			addrArr[0] = "대구광역시";
+		} else if(addrArr[0].equals("광주")) {
+			addrArr[0] = "광주광역시";
+		} else if(addrArr[0].equals("울산")) {
+			addrArr[0] = "울산광역시";
+		} else if(addrArr[0].equals("경남")) {
+			addrArr[0] = "경상남도";
+		} else if(addrArr[0].equals("경북")) {
+			addrArr[0] = "경상북도";
+		} else if(addrArr[0].equals("충북")) {
+			addrArr[0] = "충청북도";
+		} else if(addrArr[0].equals("충남")) {
+			addrArr[0] = "충청남도";
+		} else if(addrArr[0].equals("전남")) {
+			addrArr[0] = "전라남도";
+		} else if(addrArr[0].equals("전북")) {
+			addrArr[0] = "전라북도";
+		} else if(addrArr[0].equals("강원")) {
+			addrArr[0] = "강원도";
+		} 
+		
+		for(int i=0; i<addrArr.length; i++) {
+			System.out.println(addrArr[i]);
+		}
 		
 		try {
 			System.out.println(addr);
