8a3d83229f531bae1f0044a8d6402c95f90348bb
wjdwo0615 <wjd_wogns@nate.com>
2021-11-11 17:55:47 +0900

    원의방정식적용. 이제 위도 경도 리스트 뽑아볼것.

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,4 +1,73 @@
 /*
 	원의 방정식을 이용하여 주소 가져오는 js파일
+	(x-5)^2+(y-5)^2=25 ---> 지름길이 10일때 가정
+	주소기준으로 10m간격의 위도, 경도를 뽑아냄. 그 뽑아낸 위도 경도값을 array에 담아 리턴
+	
+	
+	(x-5)^2 + (y-5)^2 = 5^2
+	(x-5)^2 = 5^2 - (y-5)^2
+	x-5 = sqrt(5^2 -(y-5)^2)
+	x = sqrt(5^2 -(y-5)^2) + 5
+	
 */
+//10미터 반경 구하기 (간격1미터)
+function cycle10(){
+	var x = [];
+	var y = [];
+ 	
+ 	for(var i=0; i<5; i++){
+ 		y[i] = i+1;
+ 		x[i] = Math.sqrt(5**2 -(y[i]-5)**2) + 5;
+ 		
+ 		console.log(y[i]);
+ 		console.log(x[i]);
+ 	}
+ 	
+}
+
 //200미터 반경 구하기
+//y = 10, 20, 30, 40......
+//x = -sqrt(100^2-(y-100)^2)+100
+function cycle200(){
+	var x = [];
+	var y = [];
+ 	
+ 	for(var i=0; i<10; i++){
+ 		y[i] = 10*(i+1);
+ 		x[i] = -Math.sqrt(100**2-(y[i]-100)**2)+100;
+ 	}
+ 	
+ 	var returnArray = [];
+ 	
+ 	for(var i=0; i<10; i++){
+ 		console.log(x[i]);
+ 		returnArray[i] = 100 - x[i];
+ 		
+ 		console.log(returnArray[i]);
+ 	}
+ 	
+ 	return returnArray;
+}
+
+//500미터 반경 구하기
+//y = 10, 20, 30, 40......
+//x = -sqrt(250^2-(y-250)^2)+250
+function cycle500(){
+	var x = [];
+	var y = [];
+ 	
+ 	for(var i=0; i<25; i++){
+ 		y[i] = 10*(i+1);
+ 		x[i] = -Math.sqrt(250**2-(y[i]-250)**2)+250;
+ 	}
+ 	
+ 	
+ 	for(var i=0; i<25; i++){
+ 		console.log(x[i]);
+ 		returnArray[i] = 100 - x[i];
+ 		
+ 		console.log(returnArray[i]);
+ 	}
+ 	
+ 	return returnArray;
+}
